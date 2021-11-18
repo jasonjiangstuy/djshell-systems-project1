3f9ea754535e721f2315200d18cb9784ee8a212b
dlyalin20 <dlyalin20@stuy.edu>
2021-11-18 13:06:46 -0500

    Update shell.c
    Working on main launch loop

--- shell.c
+++ shell.c
@@ -1,3 +1,15 @@
 // Used for main shell functionality
 
 #include "shell.h"
+
+// main launch loop
+int launch_shell() {
+
+    printf("Launching shell\n");
+    while (1) {
+       printf("djshell$ ");
+       char buffer[100];
+       fgets(stdin, buffer, 100);
+    }
+
+}
