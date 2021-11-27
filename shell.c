3c25e4bd8abfae0d598717313f8eeb2c8da2268d
jasonjiangstuy <jasonjiangny@gmail.com>
2021-11-27 14:09:10 -0500

    working pclose

--- shell.c
+++ shell.c
@@ -123,28 +123,36 @@ int execute_rein(char * currentCommand, int fd) {
 }
 
 int execute_pipe(char *src, char *dest) {
+    FILE *in = popen(dest, "w");
+    int backup = dup(STDOUT_FILENO);
     int f = fork();
     if (f) {
         int status;
         wait(&status);
         int return_val = WEXITSTATUS(status);
+        dup2(backup, STDOUT_FILENO);
+
+        if (pclose(in) == -1) {
+            return -1;
+        }
         if (return_val) {
             return -1;
         }
+
         return 0;
     }
     else {
-        FILE *in = popen(dest, "w");
-        int backup = dup(STDOUT_FILENO);
         dup2(fileno(in), STDOUT_FILENO);
         char **args = parse_args(src);
         int status = execvp(args[0], args);
         if (status == 1) {
             return errno;
         }
-        dup2(backup, STDOUT_FILENO);
+
+
         return 0;
-    } 
+    }
+
 }
 
 // input make it calloc
